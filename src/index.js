24e634484dae5904f099307d5d8878011f3ebb5e
DouglasVulcano <vulcanodouglas1708@gmail.com>
2024-09-30 15:45:02 -0300

    add cors dependency

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 // Rota GET que retorna um array de valores
 app.get("/values", (req, res) => {
@@ -11,10 +14,11 @@ app.get("/values", (req, res) => {
       description: "Pais do continente americano",
     },
   ];
-  
+
   res.json(values);
 });
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
+
